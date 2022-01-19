eefbda6c2d41d040e5535fc8542dc8b5957b7ec4
ton5556 <74088689+ton5556@users.noreply.github.com>
2022-01-19 18:48:50 +0700

    Add files via upload

---
+++ ex_loadcell.ino
@@ -0,0 +1,31 @@
+#include "HX711.h"
+
+float calibration_factor =34779.00; 
+#define zero_factor 8535481
+#define DOUT  A3
+#define CLK   A2
+#define DEC_POINT  2
+
+float offset=0;
+float get_units_kg();
+
+HX711 scale(DOUT, CLK);
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Load Cell");
+  scale.set_scale(calibration_factor); 
+  scale.set_offset(zero_factor);   
+}
+void loop() 
+{ 
+  Serial.print("Reading: ");
+  String data = String(get_units_kg()+offset, DEC_POINT);
+  Serial.print(data);
+  Serial.println(" kg");
+}
+float get_units_kg()
+{
+  return(scale.get_units()*0.453592);
+}
